3b54bb6d018f55ccaaa5e6d8fd35da8010b4d288
jindulys <857367901@qq.com>
2013-10-28 15:06:53 -0400

    initial commit

---
+++ UWBookExchange/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UWBookExchange
+//
+//  Created by Li Yansong on 13-10-28.
+//  Copyright (c) 2013年 Liyansong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UWBEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UWBEAppDelegate class]));
+    }
+}
